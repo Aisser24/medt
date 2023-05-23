8f081f4ccb579e1d89684c9ab0118f90f4e8d64b
Jonathan Guger <jonathanguger@gmail.com>
2023-05-23 12:21:42 +0200

    try3

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" Component={Home}/>
-                <Route path="/about" Component={About}/>
+                <Route path="/medt/" Component={Home}/>
+                <Route path="/medt/about" Component={About}/>
                 <Route path="/new-movies" Component={New}/>
                 <Route path="/upcoming" Component={Upcoming}/>
                 <Route path="/classics" Component={Classics}/>
